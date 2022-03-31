319878d3fb77ecfbd0388f94f3f16509a0fd992b
Carina <revuwem@gmail.com>
2022-03-31 20:28:23 +0300

    feat: extract monitoring script

---
+++ public/js/monitoring.js
@@ -0,0 +1,8 @@
+window.DD_LOGS &&
+  DD_LOGS.init({
+    clientToken: "pubedf0a35240b3fb633f1c005b86c571df",
+    site: "datadoghq.com",
+    forwardErrorsToLogs: true,
+    sampleRate: 100,
+    service: "cube-examples",
+  });
